7f9e1280eb6e68644cd61eefab7a4868182a2e9e
Pernille Krejberg Petersen <pekp@itu.dk>
2024-09-10 16:06:20 +0200

    Made three new functions

--- DiningPhilosophers/philo.go
+++ DiningPhilosophers/philo.go
@@ -2,9 +2,90 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
+func randomTime(max int) {
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(max*10)))
+}
+
+func fork(leftUp, leftDown, rightUp, rightDown chan string){
+	for {
+		select {
+		case <- leftDown:
+			leftUp <- "free fork!"
+			<- leftDown
+		
+		case <- rightDown:
+			rightUp <- "free fork!"
+			<- rightDown
+		}
+	}
+}
+
+func philo(id int, leftUp, leftDown, rightUp, rightDown chan string) {
+	var philoHowFullo int
+	// Run loop until each philo has eaten 3 times
+	for philoHowFullo < 3 {
+		fmt.Printf("Philo %d thinko\n", id)
+		randomTime(2)
+		
+		// if-statement avoiding deadlocks
+		if id == 4 {
+			rightDown <- "fork free?"
+			<- rightUp
+
+			leftDown <- "fork free?"
+			<- leftUp
+		} else {
+			leftDown <- "fork free?"
+			<- leftUp
+
+			rightDown <- "fork free?"
+			<- rightUp
+		}
+
+		philoHowFullo++
+		fmt.Printf("Philo %d eato\n", id)
+		randomTime(3)
+
+		leftDown <- "Im done, delicious!"
+		rightDown <- "Im done, delicious!"
+	}
+	
+	fmt.Printf("Philo %d is fullo\n", id)
+	
+	
+	/*
+	for i := 0; i < 10; i++ {
+
+		leftDown <- "fork free?"
+		<- leftUp 
+
+		rightDown <- "fork free?"
+		<- rightUp
+
+		fmt.Println("Im eating with free forks, yummyyy")
+
+		leftDown <- "Im done, delicious!"
+		rightDown <- "Im done, delicious!"
+	}
+
+	fmt.Println("This philo is fullo")*/
+}
+
+func main() {
+
+
+}
+
+
+
+
+
+
+/*
 var p1, p2, p3, p4, p5 string 
 var f1, f2, f3, f4, f5 bool
 
@@ -37,12 +118,7 @@ func philo5(){
 		fmt.Println("eating")
 	} else {fmt.Println("thinking")}
 }
-
-func main() {
-
-
-}
-
+*/
 
 
 
