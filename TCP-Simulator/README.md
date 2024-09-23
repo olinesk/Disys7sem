42951053774676dd4822e1c12deb4ceb6142fef1
ClaraWJ <clwj@itu.dk>
2024-09-23 15:08:34 +0200

    random data generator added + error handling

--- TCP-Simulator/README.md
+++ TCP-Simulator/README.md
@@ -1,9 +1,15 @@
-# What are packages in your implementation? What data structure do you use to transmit data and meta-data?
+#### What are packages in your implementation? What data structure do you use to transmit data and meta-data?
+To verify our connections we send bools via channels. We generate random charsets of data and store this in strings and send it via string channels. 
+We have chosen only to respresent the seq-ack-seq number as metadata which is stored as integers.
 
-# Does your implementation use threads or processes? Why is it not realistic to use threads?
+#### Does your implementation use threads or processes? Why is it not realistic to use threads?
+We use threads because we are imitating a client and a server and the processes in between. It is not realistic to use threads since the protocol should run on a network, which we do not have here as we are running the 'processes' locally.
 
-# In case the network changes the order in which messages are delivered, how would you handle message re-ordering?
+#### In case the network changes the order in which messages are delivered, how would you handle message re-ordering?
+We are not handling message re-ordering, our program is designed to run first-in-first-out as of now. To handle
 
-# In case messages can be delayed or lost, how does your implementation handle message loss?
+#### In case messages can be delayed or lost, how does your implementation handle message loss?
+We have simulated a packet loss of 15%, if this is the case the error-message is printed and and the 'processes' stop. 
 
-# Why is the 3-way handshake important?
+#### Why is the 3-way handshake important?
+The 3-way handshake is important because is verifies that the client and server is connected and that data is ready to be send and received accordingly.
--- TCP-Simulator/server.go
+++ TCP-Simulator/server.go
@@ -2,59 +2,107 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
+	"math/rand"
 )
 
 
 func sendingSyn(SYN int, status chan bool) {
-	fmt.Printf("Client is sending us this: %d\n", SYN)
-	receiveSyn(SYN, status)
+	fmt.Printf("Client is sending server this: %d\n", SYN)
+	go receiveSyn(SYN, status)
 }
 
 func receiveSyn(SYN int, status chan bool) {
-	fmt.Printf("I received this from client: %d\n", SYN)
+	fmt.Printf("Server received this from client: %d\n", SYN)
 	status <- true 
 }
 
 func sendingSynAck(SYN int, status2 chan bool){
 	SYN++
-	fmt.Printf("Server is sending us this: %d\n", SYN)
-	receiveSynAck(SYN, status2)
+	fmt.Printf("Server is sending client this: %d\n", SYN)
+	go receiveSynAck(SYN, status2)
 }
 
 func receiveSynAck(SYN int, status2 chan bool){
-	fmt.Printf("I received this from server: %d\n", SYN)
+	fmt.Printf("Client received this from server: %d\n", SYN)
 	status2 <- true
 }
 
 func transferData(SYN int, data chan string){
-	var toBeSend string = "This is data, very nice yes"
-	fmt.Printf("We have this SYN: %d\n", SYN)
-	fmt.Printf("We will send this data: %s\n", toBeSend)
+	var toBeSend string = String(9)
+	fmt.Printf("Client is sending this SYN to server: %d\n", SYN)
+	fmt.Printf("Client is sending this data to server: %s\n", toBeSend)
+	go receiveData(SYN, data, toBeSend)
 }
 
-func receiveData(SYN int, data chan string, toBeSend ){
+func receiveData(SYN int, data chan string, toBeSend string){
 	data <- toBeSend
-
+	fmt.Printf("Server received this SYN from client: %d\n", SYN)
+	fmt.Printf("Server received this data from client: %s\n", toBeSend)
 }
-	
 
-func start() {
+// funtions for random string generation
+// source: https://www.calhoun.io/creating-random-strings-in-go/
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func String(length int) string {
+	return StringWithCharset(length, charset)
+}
+
+
+func start(i int) {
 	client := make(chan bool)
 	server := make(chan bool)
+	SYN := rand.Intn(100)
 
-	SYN := 100
 	go sendingSyn(SYN, client)
 	status := <- client
+
 	if status {
 		go sendingSynAck(SYN, server)
 		status2 := <- server
 		if status2 {
-			data := make(chan string)
-			go transferData(SYN+1, data)
-			status3 := <- data 
-			println(status3)
+			randomLoss := rand.Intn(15)
+			if  randomLoss == 1 {
+				fmt.Printf("packet loss on SYN: %d\n", SYN) //15% packet loss simulated
+			} else {
+				data := make(chan string)
+				go transferData(SYN+1, data)
+				status3 := <- data 
+				println(status3)
+			}
 		}
+	} else {
+		fmt.Println("Error connection status")
 	}
+
+	if i == total {
+		done <- true
+	}
+}
+
+var done chan bool
+var total int
+
+func main() {
+	done = make(chan bool)
+	total = 10
+
+	for i := 0; i <= total; i++ {
+		go start(i)
+		time.Sleep(time.Second)
+	}
+	<- done
 }
