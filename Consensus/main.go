0e7e2f103b27bf32a926120ecb14b216eb72db45
olinesk <okre@itu.dk>
2024-11-10 15:09:19 +0100

    started working on main

--- Consensus/main.go
+++ Consensus/main.go
@@ -1,2 +1,48 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	proto "github.com/olinesk/Disys7sem/proto"
+)
+
+func main() {
+
+	// Make a grpc server
+	grpcServer := grpc.NewServer()
+
+	// Make an instance of your struct
+	server := &Server {
+
+	}
+
+	// Register your server struct
+	proto.RegisterConsensusServer(grpcServer, server)
+
+	// Start serving
+	grpcServer.Serve(list)
+}
+
+type Server struct {
+	proto.UnimplementedConsensusServer
+
+}
+
+func (s *Server) GetRequest (cnx context.Context, Message *proto.Request) (*proto.Reply, error) {
+	//some code here
+    ...
+    ack :=  // make an instance of your return type
+    return (ack, nil)
+}
