ebd0b89d2d475b0184d44d5eeef93d79d1c5e320
ClaraWJ <clwj@itu.dk>
2024-11-11 12:23:46 +0100

    everybody wants to have cake, but the birthdaygirl only brought one piece, and only one guest can eat of the cake at any time

--- Consensus/main.go
+++ Consensus/main.go
@@ -1,22 +1,45 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 	"os"
-	"strings"
+	"strconv"
 	"sync"
 	"time"
 
+	"golang.org/x/net/context/ctxhttp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
 	proto "github.com/olinesk/Disys7sem/proto"
 )
+
+var grpcLog log.Logger
+var node_amount = 3
+
+type State int
+
+const (
+	StoppedEating State = iota
+	WantsToEat
+	Eating
+)
+
+type Node struct {
+	proto.UnimplementedCakeServiceServer
+	id 				int32
+	state 			State
+	time 			int32
+	timeLock		sync.Mutex
+	requestTime		int32
+	eatWG			sync.WaitGroup
+	clients 		map[int32]proto.CakeServiceClient
+	ctx				context.Context	
+}
 
 func main() {
 
@@ -39,8 +62,6 @@ type Server struct {
 	proto.UnimplementedReceiveServer
 	id int32
 	timeStamp int32
-
-
 }
 
 type Connection struct {
@@ -48,9 +69,9 @@ type Connection struct {
 	serverConnection *proto.ClientConn
 }
 
-func (s *Server) GetRequest (cnx context.Context, Message *proto.Request) (*proto.Reply, error) {
+func (s *Server) EatCake (cnx context.Context, Message *proto.Request) (*proto.Reply, error) {
 	//some code here
-    ...
+
     ack :=  // make an instance of your return type
     return (ack, nil)
 }
