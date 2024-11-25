41a66f88646daeff17be04204d961081b21ba57e
ClaraWJ <clwj@itu.dk>
2024-11-25 15:41:52 +0100

    server and main done (hopefully) - gonna test it now

--- Auction/main.go
+++ Auction/main.go
@@ -10,14 +10,18 @@ import (
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	auction "github.com/olinesk/Disys7sem/Auction/proto"
 )
 
+// FrontEnd struct represents the frontend server that interacts with multiple replication servers
+type FrontEnd struct {
+    replicationClients []auction.AuctionClient // List of clients connected to replication servers
+    auction.UnimplementedAuctionServer // Embedding to implement the AuctionServer interface
+}
 
-
-// Initialize the logger to output logs to the console
-func init(){
+func main() {
     // Set up log
 	f, err := os.OpenFile("golang-demo.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
@@ -26,14 +30,82 @@ func init(){
 	}
 	defer f.Close()
 	log.SetOutput(f)
+
+	//Setup of frontEnd clientside
+	frontEnd := &FrontEnd{}
+
+	numOfRepServs := 3
+	for i := 0; i < numOfRepServs; i++ {
+		repServPort := 5010 + (10 *i)
+
+		conn, err := grpc.Dial(fmt.Sprintf(":%v", repServPort), grpc.WithTimeout(3*time.Second), grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("Could not listen to port: %v, error: %v \n", repServPort, err)
+			continue
+		}
+		defer conn.Close()
+
+		replicationClient := auction.NewAuctionClient(conn)
+
+		frontEnd.replicationClients = append(frontEnd.replicationClients, replicationClient)
+	}
+
+	//Setup of frontEnd serverside
+	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
+	ownPort := 8010 + (arg1 * 10)
+	
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", ownPort))
+	if err != nil {
+		log.Fatalf("Error creating the server %v \n", err)
+	}
+
+	log.Printf("Starting the server on port: %d \n", ownPort)
+
+	grpcServer := grpc.NewServer()
+	auction.RegisterAuctionServer(grpcServer, frontEnd)
+	grpcServer.Serve(listener)
 }
 
-// FrontEnd struct represents the frontend server that interacts with multiple replication servers
-type FrontEnd struct {
-    replicationClient []auction.AuctionClient // List of clients connected to replication servers
-    auction.UnimplementedAuctionServer // Embedding to implement the AuctionServer interface
+func (frontEnd *FrontEnd) MakeBid(ctx context.Context, bid *auction.Bid) (*auction.Ack, error) {
+	ackChannel := make(chan *auction.Ack)
+
+	log.Printf("Bid: %d from bidder: %d \n", bid.Amount, bid.BidderId)
+
+	for index, replicationClient := range frontEnd.replicationClients {
+		go func (client auction.AuctionClient, index int) {
+			ack, err := client.MakeBid(context.Background(), bid)
+			if err != nil {
+				frontEnd.removeReplicationManager(index)
+				return
+			}
+			ackChannel <- ack
+		} (replicationClient, index)
+	}
+
+	return <-ackChannel, nil
 }
 
-func main(){
-    
+func (frontEnd *FrontEnd) GetStatus(ctx context.Context, req *auction.StatusReq) (*auction.Status, error) {
+	statusChannel := make(chan *auction.Status)
+
+	log.Println("Status requested.")
+
+	for index, replicationClient := range frontEnd.replicationClients {
+		go func (client auction.AuctionClient, index int) {
+			status, err := client.GetStatus(context.Background(), req)
+			if err != nil {
+				frontEnd.removeReplicationManager(index)
+				return
+			}
+			statusChannel <- status
+		} (replicationClient, index) 
+	}
+	
+	log.Println("Returning requested status.")
+	return <-statusChannel, nil
+}
+
+func (frontEnd *FrontEnd) removeReplicationManager(index int) {
+	log.Printf("A Replication Manager at index: %d is not responding \n", index)
+	frontEnd.replicationClients = append(frontEnd.replicationClients[:index], frontEnd.replicationClients[index+1:]...)
 }
--- Auction/server/server.go
+++ Auction/server/server.go
@@ -1,1 +1,96 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/durationpb"
+
+	auction "github.com/olinesk/Disys7sem/Auction/proto"
+)
+
+type ReplicationManager struct {
+	auction.UnimplementedAuctionServer
+	endTime 	time.Time
+	highestBid 	*auction.Bid
+	bidLock 	sync.Mutex
+}
+
+func main() {
+	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
+	ownPort := 5010 + (arg1 * 10)
+
+	replicationManager := &ReplicationManager{highestBid: &auction.Bid{BidderId: -1, Amount: 0}}
+
+	//Setup of API/FrontEnd serverside
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", ownPort))
+	if err != nil {
+		log.Fatalf("Error while creating server: %v \n", err)
+	}
+
+	log.Printf("Starting server at port: %v", ownPort)
+
+	grpcServer := grpc.NewServer()
+	auction.RegisterAuctionServer(grpcServer, replicationManager)
+	grpcServer.Serve(listener)
+}
+
+func (replicationManager *ReplicationManager) MakeBid(ctx context.Context, bid *auction.Bid) (*auction.Ack, error) {
+	if replicationManager.endTime.IsZero() {
+		auctionEndTime := bid.TimeStamp.AsTime().Add(time.Minute)
+		replicationManager.endTime = auctionEndTime
+		log.Printf("Setting end-time as: %v \n", auctionEndTime)
+	}
+
+	if replicationManager.endTime.Before(time.Now()) {
+		log.Printf("Bidder %d is trying to bid after the auction ended! \n", bid.BidderId)
+		return &auction.Ack{}, nil
+	}
+
+	if replicationManager.setHighestBid(bid) {
+		log.Printf("Bidder %d has the highest bid of: %d monnays \n", bid.BidderId, bid.Amount)
+	} else {
+		log.Printf("The bid %d from bidder %d was too low. \n", bid.Amount, bid.BidderId)
+	}
+
+	return &auction.Ack{}, nil
+}
+
+func (replicationManager *ReplicationManager) GetStatus (ctx context.Context, req *auction.StatusReq) (*auction.Status, error) {
+	log.Printf("Bidder: %d is requesting a status of the auction. \n", req.BidderId)
+
+	highestBid := replicationManager.getHighestBid()
+	timeLeft := time.Until(replicationManager.endTime)
+
+	status := &auction.Status{
+		TimeLeft: durationpb.New(timeLeft),
+		HighestBid: highestBid.Amount,
+		BidderId: highestBid.BidderId,
+	}
+
+	return status, nil
+}
+
+func (replicationManager *ReplicationManager) setHighestBid (bid *auction.Bid) bool {
+	replicationManager.bidLock.Lock()
+	defer replicationManager.bidLock.Unlock()
+	if bid.Amount < replicationManager.highestBid.Amount {
+		return false
+	}
+	replicationManager.highestBid = bid
+	return true
+}
+
+func (replicationManager *ReplicationManager) getHighestBid() *auction.Bid {
+	replicationManager.bidLock.Lock()
+	defer replicationManager.bidLock.Unlock()
+	return replicationManager.highestBid
+}
