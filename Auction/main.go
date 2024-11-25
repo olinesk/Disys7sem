87ead21e45a665321e40c7fb5c2a4f663e74d914
ClaraWJ <clwj@itu.dk>
2024-11-25 12:45:45 +0100

    started making main

--- Auction/main.go
+++ Auction/main.go
@@ -1,2 +1,21 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"time"
+	"log"
+
+	"google.golang.org/grpc"
+
+	auction "github.com/olinesk/Disys7sem/Auction/proto"
+)
+
+func init() {
+	
+}
+
+
